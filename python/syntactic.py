63b2fd25aca3f72fd5bc65485d7c598d6ae55a3c
Rafael S. Campos <rafael.schubert.campos@gmail.com>
2020-02-24 22:34:42 -0300

    Parser's initial flow
    Initial parsing flow of Parser according to the grammar.

--- python/syntactic.py
+++ python/syntactic.py
@@ -1,2 +1,37 @@
+import lexic
+
 class Parser():
-    pass
+    def __init__(self):
+        self._tokenizer = lexic.Tokenizer()
+
+    def parse(self, text):
+        self._tokenizer.parse(text)
+
+        self._rollexpr()
+
+    def _rollexpr(self):
+        self._sumorsubexpr()
+
+    def _sumorsubexpr(self):
+        self._dicecomboexpr()
+        self._optionalsumorsubrighthandexpr()
+
+    def _dicecomboexpr(self):
+        self._optionaldiceexpr()
+
+    def _optionaldiceexpr(self):
+        self._diceexpr()
+
+    def _diceexpr(self):
+        pass
+
+    def _optionalsumorsubrighthandexpr(self):
+        self._plusorminusexpr()
+        self._sumorsubexpr()
+
+    def _plusorminusexpr(self):
+        pass
+
+if __name__ == '__main__':
+    my_parser = Parser()
+    # my_parser.parse('2d10')
