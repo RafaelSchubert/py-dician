e0dadac315b4c7538490591b9b56ba601b0fb30c
Rafael S. Campos <rafael.schubert.campos@gmail.com>
2020-03-04 23:18:35 -0300

    Parse the expression syntactically.
    Check only. No op tree for now.

--- python/syntactic.py
+++ python/syntactic.py
@@ -3,35 +3,94 @@ import lexic
 class Parser():
     def __init__(self):
         self._tokenizer = lexic.Tokenizer()
+        self._token     = None
 
     def parse(self, text):
         self._tokenizer.parse(text)
 
-        self._rollexpr()
+        self._nexttoken()
+
+        if self._token.kind == lexic.TokenType.END:
+            return True
+
+        return self._rollexpr()
 
     def _rollexpr(self):
-        self._sumorsubexpr()
+        return self._sumorsubexpr()
 
     def _sumorsubexpr(self):
-        self._dicecomboexpr()
-        self._optionalsumorsubrighthandexpr()
+        if self._dicecomboexpr():
+            return self._sumorsubrighthandexpr()
+
+        return False
 
     def _dicecomboexpr(self):
-        self._optionaldiceexpr()
+        if self._token.kind != lexic.TokenType.INTEGER:
+            return False
 
-    def _optionaldiceexpr(self):
-        self._diceexpr()
+        self._nexttoken()
+
+        return self._dicecomborighthandexpr()
+
+    def _dicecomborighthandexpr(self):
+        if self._token.kind == lexic.TokenType.END:
+            return True
+
+        if self._tokenisplusorminus():
+            return True
+
+        return self._diceexpr()
 
     def _diceexpr(self):
-        pass
+        if self._token.kind != lexic.TokenType.KW_D:
+            return False
 
-    def _optionalsumorsubrighthandexpr(self):
-        self._plusorminusexpr()
-        self._sumorsubexpr()
+        self._nexttoken()
+
+        if self._token.kind != lexic.TokenType.INTEGER:
+            return False
+
+        self._nexttoken()
+
+        return True
+
+    def _sumorsubrighthandexpr(self):
+        if self._token.kind == lexic.TokenType.END:
+            return True
+
+        if self._plusorminusexpr():
+            return self._sumorsubexpr()
+
+        return False
 
     def _plusorminusexpr(self):
-        pass
+        if self._tokenisplusorminus():
+            self._nexttoken()
+
+            return True
+
+        return False
+
+    def _tokenisplusorminus(self):
+        if self._token.kind == lexic.TokenType.SB_PLUS:
+            return True
+
+        if self._token.kind == lexic.TokenType.SB_MINUS:
+            return True
+
+        return False
+
+    def _nexttoken(self):
+        self._token = self._tokenizer.fetch()
 
 if __name__ == '__main__':
-    my_parser = Parser()
-    # my_parser.parse('2d10')
+    expression = '1d6 + 1d6'
+    my_parser  = Parser()
+
+    print(f'Is "{expression}" a valid roll expression?')
+
+    try:
+        print('Yes.' if my_parser.parse(expression) else 'No.')
+    except lexic.TokenizerError as e:
+        print(f'Nope. Something went wrong!')
+        print(f'--> {e}')
