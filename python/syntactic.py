50aeb93fca92ea7798bb670f551ff43f100ab0ce
Rafael S. Campos <rafael.schubert.campos@gmail.com>
2020-03-16 23:13:15 -0300

    Refactoring syntactic.py.
    Applying Python code style guide conventions.
    Simplifying the Parser class.

--- python/syntactic.py
+++ python/syntactic.py
@@ -1,129 +1,153 @@
 import lexic
 
+
 class ParserError(Exception):
     pass
 
+
 class UnexpectedTokenError(ParserError):
-    def __init__(self, found_token, expected_token_type = None):
-        self.found_token         = found_token
+    def __init__(self, found_token, expected_token_type=None):
+        self.found_token = found_token
         self.expected_token_type = expected_token_type
+
+    def __repr__(self):
+        return f'{self.__class__.__name__}(found_token="{self.found_token}", expected_token_type={self.expected_token_type})'
 
     def __str__(self):
         message = f'An unexpected token was found. Found = {self.found_token}.'
+
         if self.expected_token_type == None:
             return message
+
         return message + f' Expected = {self.expected_token_type}.'
+
 
 class Parser():
     def __init__(self):
         self._tokenizer = lexic.Tokenizer()
-        self._token     = None
+        self._current_token = None
 
-    def parse(self, text):
-        self._tokenizer.set_input_string(text)
-        self._nexttoken()
-        if self._token.kind == lexic.TokenType.END:
+    def parse(self, input_string):
+        self._tokenizer.set_input_string(input_string)
+
+        self._next_token()
+
+        if self._expect_token_is_any_of(lexic.TokenType.END):
             return True
-        return self._rollexpr()
 
-    def _rollexpr(self):
-        return self._arithmeticexpr()
+        return self._roll_expression()
 
-    def _arithmeticexpr(self):
-        return self._sumorsubexpr()
+    def _roll_expression(self):
+        return self._arithmetic_expression()
 
-    def _sumorsubexpr(self):
-        if self._prodordivexpr():
-            return self._sumorsubrighthandexpr()
+    def _arithmetic_expression(self):
+        return self._addition_or_subtraction()
+
+    def _addition_or_subtraction(self):
+        if self._product_or_division():
+            return self._addition_or_subtraction_right_hand()
+
         return False
 
-    def _sumorsubrighthandexpr(self):
-        if not self._plusorminusexpr():
+    def _addition_or_subtraction_right_hand(self):
+        if not self._plus_or_minus():
             return True
-        if not self._prodordivexpr():
-            raise UnexpectedTokenError(self._token)
-        return self._sumorsubrighthandexpr()
 
-    def _plusorminusexpr(self):
-        return self._consumeanyof(lexic.TokenType.PLUS, lexic.TokenType.MINUS)
+        if not self._product_or_division():
+            self._raise_unexpected_token_error()
 
-    def _prodordivexpr(self):
-        if self._signaledvalueexpr():
-            return self._prodordivrighthandexpr()
+        return self._addition_or_subtraction_right_hand()
+
+    def _plus_or_minus(self):
+        return self._expect_token_is_any_of(lexic.TokenType.PLUS, lexic.TokenType.MINUS)
+
+    def _product_or_division(self):
+        if self._signaled_value():
+            return self._product_or_division_right_hand()
+
         return False
 
-    def _prodordivrighthandexpr(self):
-        if not self._multordivexpr():
+    def _product_or_division_right_hand(self):
+        if not self._multiply_or_divide():
             return True
-        if not self._signaledvalueexpr():
-            raise UnexpectedTokenError(self._token)
-        return self._prodordivrighthandexpr()
 
-    def _multordivexpr(self):
-        return self._consumeanyof(lexic.TokenType.MULTIPLY, lexic.TokenType.DIVIDE)
+        if not self._signaled_value():
+            self._raise_unexpected_token_error()
 
-    def _signaledvalueexpr(self):
-        if not self._plusorminusexpr():
-            return self._valueexpr()
-        if self._valueexpr():
+        return self._product_or_division_right_hand()
+
+    def _multiply_or_divide(self):
+        return self._expect_token_is_any_of(lexic.TokenType.MULTIPLY, lexic.TokenType.DIVIDE)
+
+    def _signaled_value(self):
+        if not self._plus_or_minus():
+            return self._value_expression()
+
+        if self._value_expression():
             return True
-        raise UnexpectedTokenError(self._token)
 
-    def _valueexpr(self):
-        return self._dicesetexpr()
+        self._raise_unexpected_token_error()
 
-    def _dicesetexpr(self):
-        if self._numberexpr():
-            return self._dicesetrighthandexpr()
+    def _value_expression(self):
+        return self._dice_set_expression()
+
+    def _dice_set_expression(self):
+        if self._number_expression():
+            return self._dice_set_expression_right_hand()
+
         return False
 
-    def _dicesetrighthandexpr(self):
-        self._dieexpr()
+    def _dice_set_expression_right_hand(self):
+        self._die_expression()
         return True
 
-    def _numberexpr(self):
-        if self._parenthesizedexpr():
+    def _number_expression(self):
+        if self._parenthesized_expression():
             return True
-        if self._dieexpr():
-            return True
-        return self._integerexpr()
 
-    def _parenthesizedexpr(self):
-        if not self._leftparenthesisexpr():
+        if self._die_expression():
+            return True
+
+        return self._expect_token_is_any_of(lexic.TokenType.INTEGER)
+
+    def _parenthesized_expression(self):
+        if not self._expect_token_is_any_of(lexic.TokenType.LEFT_PARENTHESIS):
             return False
-        if not self._arithmeticexpr():
-            raise UnexpectedTokenError(self._token)
-        if self._rightparenthesisexpr():
+
+        if not self._arithmetic_expression():
+            self._raise_unexpected_token_error()
+
+        if self._expect_token_is_any_of(lexic.TokenType.RIGHT_PARENTHESIS):
             return True
-        raise UnexpectedTokenError(self._token)
 
-    def _leftparenthesisexpr(self):
-        return self._consumeanyof(lexic.TokenType.LEFT_PARENTHESIS)
+        self._raise_unexpected_token_error(lexic.TokenType.RIGHT_PARENTHESIS)
 
-    def _rightparenthesisexpr(self):
-        return self._consumeanyof(lexic.TokenType.RIGHT_PARENTHESIS)
-
-    def _dieexpr(self):
-        if not self._dietagexpr():
+    def _die_expression(self):
+        if not self._expect_token_is_any_of(lexic.TokenType.DIE):
             return False
-        if self._numberexpr():
+
+        if self._number_expression():
             return True
-        raise UnexpectedTokenError(self._token)
 
-    def _dietagexpr(self):
-        return self._consumeanyof(lexic.TokenType.DIE)
+        self._raise_unexpected_token_error()
 
-    def _integerexpr(self):
-        return self._consumeanyof(lexic.TokenType.INTEGER)
+    def _expect_token_is_any_of(self, *expected_token_types):
+        return self._expect_token_is(lambda token: token.kind in expected_token_types)
 
-    def _consumeanyof(self, *expected_token_types):
-        if not self._token.kind in expected_token_types:
+    def _expect_token_is(self, condition):
+        if not condition(self._current_token):
             return False
-        self._nexttoken()
+
+        self._next_token()
+
         return True
 
-    def _nexttoken(self):
-        self._token = self._tokenizer.next_token()
+    def _next_token(self):
+        self._current_token = self._tokenizer.next_token()
+
+    def _raise_unexpected_token_error(self, expected_token_type=None):
+        raise UnexpectedTokenError(self._current_token, expected_token_type)
+
 
 if __name__ == '__main__':
     expression = '3 * +1 / 2d6 - 2 + 1dd6 / -(2 + 1)d10 - 5 + +d4d8 * dd3dd12'
