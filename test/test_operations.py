89fc0dda69b48aa4564046eaacbc592c37da1ba4
Rafael S. Campos <rafael.schubert.campos@gmail.com>
2020-07-01 19:17:11 -0300

    Formatting test code.

--- test/test_operations.py
+++ test/test_operations.py
@@ -10,69 +10,92 @@ class TestOperation(unittest.TestCase):
 
 class TestLiteralValueOp(unittest.TestCase):
     def test_returns_unmodified(self):
-        test_values = [None, True, False, 0, 1, -1,
-                       0.5, -0.5, 'Something', 'Something else']
+        test_values = [
+                None,
+                True,
+                False,
+                0,
+                1,
+                -1,
+                0.5,
+                -0.5,
+                'Something',
+                'Something else'
+            ]
         for tv in test_values:
             self.assertEqual(pydician.LiteralValueOp(tv).run(), tv)
 
 
 class TestDieOp(unittest.TestCase):
     def test_returns_callable(self):
-        self.assertTrue(callable(pydician.DieOp(pydician.LiteralValueOp(6)).run()))
+        op = pydician.DieOp(pydician.LiteralValueOp(6))
+        self.assertTrue(callable(op.run()))
 
     def test_die_generates_int(self):
-        die = pydician.DieOp(pydician.LiteralValueOp(6)).run()
+        op = pydician.DieOp(pydician.LiteralValueOp(6))
+        die = op.run()
         self.assertEqual(type(die()), int)
 
 
 class TestDiceRollOp(unittest.TestCase):
     def test_returns_int(self):
-        result = pydician.DiceRollOp(pydician.LiteralValueOp(2),
-                                     pydician.DieOp(pydician.LiteralValueOp(6))).run()
+        op = pydician.DiceRollOp(pydician.LiteralValueOp(2),
+                                 pydician.DieOp(pydician.LiteralValueOp(6)))
+        result = op.run()
         self.assertEqual(type(result), int)
 
 
 class TestSingleDieRollOp(unittest.TestCase):
     def test_returns_int(self):
-        result = pydician.SingleDieRollOp(pydician.DieOp(pydician.LiteralValueOp(6))).run()
+        op = pydician.SingleDieRollOp(pydician.DieOp(pydician.LiteralValueOp(6)))
+        result = op.run()
         self.assertEqual(type(result), int)
 
 
 class _TestArithmeticOp(unittest.TestCase):
-    _TEST_VALUES = [0, 1, -1, 0.5, -0.5, 1.5, -1.5]
+    _TEST_VALUES = [
+            0,
+            1,
+            -1,
+            0.5,
+            -0.5,
+            1.5,
+            -1.5
+        ]
 
 
 class TestNegateOp(_TestArithmeticOp):
     def test_returns_arithmetic_negate(self):
         for tv in super()._TEST_VALUES:
-            self.assertEqual(pydician.NegateOp(pydician.LiteralValueOp(tv)).run(), -tv)
+            op = pydician.NegateOp(pydician.LiteralValueOp(tv))
+            self.assertEqual(op.run(), -tv)
 
 
 class TestSumOp(_TestArithmeticOp):
     def test_returns_sum(self):
         for ltv in super()._TEST_VALUES:
             for rtv in super()._TEST_VALUES:
-                self.assertEqual(pydician.SumOp(pydician.LiteralValueOp(ltv),
-                                                pydician.LiteralValueOp(rtv)).run(),
-                                 ltv+rtv)
+                op = pydician.SumOp(pydician.LiteralValueOp(ltv),
+                                    pydician.LiteralValueOp(rtv))
+                self.assertEqual(op.run(), ltv+rtv)
 
 
 class TestSubtractOp(_TestArithmeticOp):
     def test_returns_subtraction(self):
         for ltv in super()._TEST_VALUES:
             for rtv in super()._TEST_VALUES:
-                self.assertEqual(pydician.SubtractOp(pydician.LiteralValueOp(ltv),
-                                                     pydician.LiteralValueOp(rtv)).run(),
-                                 ltv-rtv)
+                op = pydician.SubtractOp(pydician.LiteralValueOp(ltv),
+                                         pydician.LiteralValueOp(rtv))
+                self.assertEqual(op.run(), ltv-rtv)
 
 
 class TestMultiplyOp(_TestArithmeticOp):
     def test_returns_multiplication(self):
         for ltv in super()._TEST_VALUES:
             for rtv in super()._TEST_VALUES:
-                self.assertEqual(pydician.MultiplyOp(pydician.LiteralValueOp(ltv),
-                                                     pydician.LiteralValueOp(rtv)).run(),
-                                 ltv*rtv)
+                op = pydician.MultiplyOp(pydician.LiteralValueOp(ltv),
+                                         pydician.LiteralValueOp(rtv))
+                self.assertEqual(op.run(), ltv*rtv)
 
 
 class TestDivideOp(_TestArithmeticOp):
