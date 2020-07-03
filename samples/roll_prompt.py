74ff9b48d79e8c8cad778f1e398315db792dd650
Rafael S. Campos <rafael.schubert.campos@gmail.com>
2020-07-03 19:06:24 -0300

    Record last successful roll expression.
    Use the last successful roll expression if no input is provided.

--- samples/roll_prompt.py
+++ samples/roll_prompt.py
@@ -4,14 +4,24 @@ import pydician
 def main():
     print('Input a Py-Dician roll expression to be parsed ("q" to exit):')
 
+    last_roll_expr = None
+
     while True:
         try:
             roll_expr = input('Roll >> ').strip()
+
             if not roll_expr:
-                continue
+                if last_roll_expr:
+                    roll_expr = last_roll_expr
+                else:
+                    continue
+
             if roll_expr.lower()=='q':
                 break
+
             result = pydician.roll(roll_expr)
+            last_roll_expr = roll_expr
+
             print(f'{roll_expr} = {result}')
         except pydician.PyDicianError:
             print(f'"{roll_expr}" is not recognized as a roll expression.')
